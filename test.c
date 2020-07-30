933e25224a4ddb5a04d2b56473c5f9b3c016c31b
Fellipe Augusto Ugliara <ugliara.fellipe@gmail.com>
2020-07-30 17:22:10 -0300

    test: Create a main to call the test functions

---
+++ test.c
@@ -0,0 +1,29 @@
+//
+// Copyright (c) 2020, Fellipe Augusto Ugliara
+//
+// Use of this source code is governed by an ISC license that can be found
+// in the LICENSE file at https://github.com/ugliara-fellipe/library.datatype
+//
+#include "function.h"
+#include "graph.h"
+#include "list.h"
+#include "logic.h"
+#include "nil.h"
+#include "number.h"
+#include "pair.h"
+#include "pointer.h"
+#include "table.h"
+#include "text.h"
+
+int main() {
+  function_test();
+  graph_test();
+  list_test();
+  logic_test();
+  nil_test();
+  number_test();
+  pair_test();
+  pointer_test();
+  table_test();
+  text_test();
+}
